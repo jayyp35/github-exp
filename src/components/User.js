aeba2ab74c912d6884b5eadfae55dcf01c3ee318
jayyp35 <jprabhu35@gmail.com>
2021-03-05 00:28:49 +0530

    auto load

--- src/components/User.js
+++ src/components/User.js
@@ -4,8 +4,9 @@ const User = ({user,click}) => {
     return(
         <div className="card m-2">
             <div className="card-body text-center border border-5 border-dark rounded m-2 bg-secondary">
-                <img src={user.avatar_url} alt="Img"/>
+                <img className="border border-info m-2" src={user.avatar_url} alt="Img"/>
                 <h1>{user.name}</h1>
+                <hr></hr>
                 <p>{user.company}</p>
                 <p>{user.bio}</p>
             </div>
